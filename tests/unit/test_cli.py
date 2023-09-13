b21df74ee119c2c2e6e67cd45cae601a9080ed39
John Emil Hallelland <joemha@equinor.com>
2023-09-13 11:32:57 +0000

    upd test 2

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -13,9 +13,9 @@ def test_that_cli_fails_when_file_is_missing():
 def test_that_cli_prints_coordinates(monkeypatch, tmp_path):
     monkeypatch.chdir(tmp_path)
     expected_coordinates_list = [
-        ("2.0", "1.9"),
-        ("2.5", "1.5"),
-        ("3.3", "7.3"),
+        (2.0, 1.9),
+        (2.5, 1.5),
+        (3.3, 7.3),
     ]
     with open("test_input", "w", encoding="utf-8") as fout:
         fout.write("\n".join(",".join(coords) for coords in expected_coordinates_list))
@@ -23,5 +23,5 @@ def test_that_cli_prints_coordinates(monkeypatch, tmp_path):
     result = runner.invoke(main, ["test_input"])
     assert result.exit_code == 0
     assert (
-        "coordinates: [('2.0', '1.9'), ('2.5', '1.5'), ('3.3', '7.3')]" in result.output
+        "coordinates: [(2.0, 1.9), (2.5, 1.5), (3.3, 7.3)]" in result.output
     )
